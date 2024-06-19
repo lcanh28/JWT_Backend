79f3fb5d41aa4c6c5b906012c20d7b98418c8aa9
lcanh28 <lcanh2003@gmail.com>
2024-06-19 10:54:21 +0700

    CRUD basic with data

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,9 +1,15 @@
 import express from 'express';
+import homeController from '../controller/homeController';
 
 const router = express.Router();
 
 const initWebRoutes = (app) => {
-    router.get('/', (req, res) => {});
+    router.get('/', homeController.handleHello);
+    router.get('/user', homeController.handleUserPage);
+    router.post('/users/create-user', homeController.handleCreateUser);
+    router.post('/delete-user/:id', homeController.handleDeleUser);
+    router.get('/update-user/:id', homeController.getUpdateUserPage);
+    router.post('/user/update-user', homeController.handleUpdateUser);
 
     return app.use('/', router);
 };
