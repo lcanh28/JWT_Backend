0fa1909dfd42d308b5d66997d7dd64c0db902dc7
lcanh28 <lcanh2003@gmail.com>
2025-02-23 22:27:56 +0700

    code service getAllUser

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -2,7 +2,13 @@ import userApiService from '../service/userApiService'
 
 const readFunc = async (req, res) => {
     try{
-        let users = await userApiService.getAllUser();
+        let data = await userApiService.getAllUser();
+
+        return res.status(200).json({
+            EM: data.EM, //error message
+            EC: data.EC, //error code
+            DT: data.DT, //data
+        });
 
     }catch(e){
         console.log(e)
--- src/service/userApiService.js
+++ src/service/userApiService.js
@@ -1,17 +1,85 @@
 import bcrypt from 'bcryptjs';
 import db from '../models/index';
+import { where } from 'sequelize/lib/sequelize';
 
-const getAllUser = () => {
+const getAllUser = async () => {
+    try {
+        let users = await db.User.findAll({
+            attributes: ['id', 'username', 'email', 'phone', 'sex'],
+            include: { model: db.Group, attributes: ['name', 'description'] },
+            raw: true,
+            nest: true,
+        });
+        if(users) {
+            return {
+                EM: 'success',
+                EC: 0,
+                DT: users
+            }
+        } else {
+            return {
+                EM: 'success',
+                EC: 0,
+                DT: []
+            }
+        }
+    } catch(e) {
+        console.log(e);
+        return {
+            EM: 'Something wrongs in services....',
+            EC: 0,
+            DT: []
+        }
+    }
+}
+const createNewUser = async (data) => {
+    try {
+        await db.User.create({
 
+        })
+    } catch(e) {
+        console.log(e);
+        return {
+            EM: 'Something wrongs in services....',
+            EC: 0,
+            DT: []
+        }
+    }
 }
-const createNewUser = () => {
-    
+const updateUser = async (data) => {
+    try {
+        let user = await db.User.findOne({
+            where: {id: data.id}
+        })
+        if(user) {
+            user.save({
+
+            })
+        } else {
+
+        }
+    } catch(e) {
+        console.log(e);
+        return {
+            EM: 'Something wrongs in services....',
+            EC: 0,
+            DT: []
+        }
+    }
 }
-const updateUser = () => {
-    
-}
-const deleteUser = () => {
-    
+const deleteUser = async (id) => {
+    try {
+        await db.User.delete({
+            where: {id : data.id}
+        })
+    } catch(e) {
+        console.log(e);
+        return {
+            EM: 'Something wrongs in services....',
+            EC: 0,
+            DT: []
+        }
+    }
 }
 
 module.exports = { getAllUser, createNewUser, updateUser, deleteUser }
